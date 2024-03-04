2b00a4c0be4406bab45209b0d67399f8b8a6026a
Usama <usama-365@protonmail.com>
2024-03-04 14:56:59 +0500

    create base routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+
+import SignInPage from "./pages/public/auth/SignInPage.tsx";
+import SignUpPage from "./pages/public/auth/SignUpPage.tsx";
+import AuthLayout from "./pages/public/auth/AuthLayout.tsx";
+import HomePage from "./pages/private/HomePage.tsx";
+import RootLayout from "./pages/private/RootLayout.tsx";
+
 export default function App() {
-  return <p className="bg-red-500">Hello World</p>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path={"/sign-in"} element={<SignInPage />} />
+          <Route path={"/sign-up"} element={<SignUpPage />} />
+        </Route>
+
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 }
